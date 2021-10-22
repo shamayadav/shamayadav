35959868858db28c2a341b06ba462ed764559653
shamayadav <92978582+shamayadav@users.noreply.github.com>
2021-10-22 14:20:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,21 @@
+provider "google" {
+  project     = "terraform-up-and-running-code"
+  # credentials = GOOGLE_CREDENTIALS
+  region = "us-central1"
+}
 
+resource "google_compute_instance" "example" {
+  name = "example"
+  machine_type  = "f1-micro"
+  zone = "us-central1-a"
+  disk {
+    image = "ubuntu-1604-lts"
+  }
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
