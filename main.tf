bf80720b49294f925e94d6e8a4b9836db0a37d64
shamayadav <92978582+shamayadav@users.noreply.github.com>
2021-10-29 10:03:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,69 @@
 provider "google" {
-  project     = "terraform-up-and-running-code"
-  # credentials = GOOGLE_CREDENTIALS
-  region = "us-central1"
+    project = "hsbc-329610"
+    region = "europe-west2"
+    zone = "europe-west2-c"
+    credentials = "credential.json"
 }
 
-resource "google_compute_instance" "example" {
-  name = "example"
-  machine_type  = "f1-micro"
-  zone = "us-central1-a"
-  disk {
-    image = "ubuntu-1604-lts"
-  }
-  network_interface {
-    network = "default"
 
-    access_config {
-      // Ephemeral IP
+terraform {
+  backend "gcs"{
+      bucket="tf-state-om-tech"
+      prefix="terraform/state"
+  }
+}
+
+
+
+resource "google_compute_instance" "template-vm" {
+  name="template-vm"
+  machine_type="e2-medium"
+
+  network_interface {
+    network="default"
+  }
+
+  boot_disk {
+    initialize_params{
+        image="rhel-cloud/rhel-8"
     }
   }
+    tags = ["image-template",
+    "rhel-image"]
+
+
 }
+/*
+resource "google_compute_instance" "image-vm" {
+  name="image-vm"
+  machine_type="e2-medium"
+
+  network_interface {
+    network="hsbc-shared-vpc"
+  }
+
+  boot_disk {
+    source = google_compute_disk.f-disk.name
+  }
+    
+}
+
+resource "google_compute_disk" "f-disk" {
+  name="image-vm-vol"
+  image=resource.google_compute_machine_image.rhel-image.self_link
+  type="ps-ssd"
+  size=100
+
+
+}
+
+#data "google_compute_image" "t-google-compute-image" {
+#    source=google_compute_machine_image.rhel-image.name
+#}
+
+resource "google_compute_machine_image" "rhel-image" {
+  provider        = google-beta
+  name="rhel-image"
+  source_instance= google_compute_instance.template-vm.self_link
+}
+*/
