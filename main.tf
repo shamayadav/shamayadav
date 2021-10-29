06d1b35621e38e4c5967fb141b4887ebe362cdac
shamayadav <92978582+shamayadav@users.noreply.github.com>
2021-10-29 10:15:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "google" {
-    project = "hsbc-329610"
+    project = "zeta-bebop-281216"
     region = "europe-west2"
     zone = "europe-west2-c"
     credentials = "credential.json"
 }
 
-
+/*
 terraform {
   backend "gcs"{
       bucket="tf-state-om-tech"
@@ -13,11 +13,11 @@ terraform {
   }
 }
 
+*/
 
-
-resource "google_compute_instance" "template-vm" {
-  name="template-vm"
-  machine_type="e2-medium"
+resource "google_compute_instance" "vm_git_jenkins_cicd" {
+  name="vm_git_jenkins_cicd"
+  machine_type="f1-micro"
 
   network_interface {
     network="default"
@@ -33,37 +33,3 @@ resource "google_compute_instance" "template-vm" {
 
 
 }
-/*
-resource "google_compute_instance" "image-vm" {
-  name="image-vm"
-  machine_type="e2-medium"
-
-  network_interface {
-    network="hsbc-shared-vpc"
-  }
-
-  boot_disk {
-    source = google_compute_disk.f-disk.name
-  }
-    
-}
-
-resource "google_compute_disk" "f-disk" {
-  name="image-vm-vol"
-  image=resource.google_compute_machine_image.rhel-image.self_link
-  type="ps-ssd"
-  size=100
-
-
-}
-
-#data "google_compute_image" "t-google-compute-image" {
-#    source=google_compute_machine_image.rhel-image.name
-#}
-
-resource "google_compute_machine_image" "rhel-image" {
-  provider        = google-beta
-  name="rhel-image"
-  source_instance= google_compute_instance.template-vm.self_link
-}
-*/
