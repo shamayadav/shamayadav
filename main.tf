470b41ce59f930fff148e509db72dd020645c056
shamayadav <92978582+shamayadav@users.noreply.github.com>
2021-11-01 17:50:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,9 +29,9 @@ resource "google_compute_instance" "vm1" {
     }
   }
     tags = ["image-template",
-    "rhel-image5"]
-    
-   metadata = {
+    "rhel-image"]
+
+    metadata = {
     startup-script = <<-EOF
     sudo su -
     useradd mgmtbld
@@ -40,6 +40,69 @@ resource "google_compute_instance" "vm1" {
     echo "%mgmtbld ALL=(ALL:ALL) NOPASSWD:ALL">mgmtbld
   EOF
   }
+}
+#Second VM--
+resource "google_compute_instance" "vm2" {
+  name="vm2"
+  machine_type="f1-micro"
 
+  network_interface {
+    network="default"
+  }
 
+  boot_disk {
+    initialize_params{
+        image="rhel-cloud/rhel-8"
+    }
+  }
+    tags = ["image-template",
+    "rhel-image"]
+
+    metadata = {
+    startup-script = <<-EOF
+    sudo su -
+    useradd mgmtbld
+    touch /etc/sudoers.d/mgmtbld
+    cd /etc/sudoers.d
+    echo "%mgmtbld ALL=(ALL:ALL) NOPASSWD:ALL">mgmtbld
+  EOF
+  }
+}
+#Third VM--
+resource "google_compute_instance" "vm3" {
+  name="vm3"
+  machine_type="f1-micro"
+
+  network_interface {
+    network="default"
+  }
+
+  boot_disk {
+    initialize_params{
+        image="rhel-cloud/rhel-8"
+    }
+  }
+    tags = ["image-template",
+    "rhel-image"]
+
+    metadata = {
+    startup-script = <<-EOF
+    sudo su -
+    useradd mgmtbld
+    touch /etc/sudoers.d/mgmtbld
+    cd /etc/sudoers.d
+    echo "%mgmtbld ALL=(ALL:ALL) NOPASSWD:ALL">mgmtbld
+  EOF
+  }
+}
+output "vm-address1" {
+  value=google_compute_instance.vm1.network_interface.0.network_ip
+}
+
+output "vm-address2" {
+  value=google_compute_instance.vm2.network_interface.0.network_ip
+}
+
+output "vm-address3" {
+  value=google_compute_instance.vm3.network_interface.0.network_ip
 }
